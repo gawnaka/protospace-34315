25bc9f46bb3c57293e4fbb21aff2f0f61cc39d5e
gawnaka <gawnaka0828@gmail.com>
2021-05-03 13:03:23 +0900

    Update prototypes_controller.rb
    H

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -50,7 +50,7 @@ class PrototypesController < ApplicationController
          private
   
         def prototype_params
-        params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: :current_user.id)
+        params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
         end
         
 end
